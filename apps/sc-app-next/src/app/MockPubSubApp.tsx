4045ba723381afbc6464da1a5e715b37f3e58a72
Vitalie Andries <vitalie.andries@gmail.com>
2022-12-26 16:55:31 +0200

    phase: reuse : Next app - customizing

---
+++ apps/sc-app-next/src/app/MockPubSubApp.tsx
@@ -0,0 +1,79 @@
+import React, { ReactElement, useState } from 'react';
+import { MockBet, MockEvent } from '@sc-app-legacy/test/MockEvent';
+import reject from 'lodash/reject';
+import { MyEvents } from '@sc-app-legacy/test/MyEvents';
+import { MyEventDetails } from '@sc-app-legacy/test/MyEventDetails';
+
+import { MyBets } from './MyBets';
+
+export function MockContent({ fooParam }: { fooParam: MockEvent[] }): ReactElement {
+    const [events] = useState<MockEvent[]>(fooParam);
+    const [bets, setBets] = useState<MockBet[]>([]);
+    const [betId, setBetId] = useState(0);
+
+    const [state, setState] = useState<{
+        currentEvent: null | MockEvent;
+        listVisible: boolean;
+    }>({
+        currentEvent: null,
+        listVisible: true,
+    });
+
+    const { currentEvent, listVisible } = state;
+
+    function setCurrentEvent(currentEvent: MockEvent | null) {
+        setState({
+            ...state,
+            listVisible: currentEvent === null,
+            currentEvent,
+        });
+    }
+
+    // const [list, toggleList] = useState(false);
+
+    function placeBet(event: MockEvent): void {
+        let aBet = { event, id: betId };
+
+        setBets([...bets, aBet]);
+        setBetId(betId + 1);
+    }
+
+    function showEventDetails(event: MockEvent) {
+        setCurrentEvent(event);
+    }
+
+    function removeBar(id: number) {
+        const reducedBars = reject(bets, { id });
+
+        setBets(reducedBars);
+    }
+
+    function toggleList() {
+        setState({
+            ...state,
+            listVisible: !listVisible,
+        });
+    }
+
+    return (
+        <div>
+            {!currentEvent && <button onClick={toggleList}>show list</button>}
+            {currentEvent && (
+                <MyEventDetails
+                    event={currentEvent}
+                    src="details"
+                    handleDone={() => setCurrentEvent(null)}
+                    handleBet={placeBet}
+                />
+            )}
+
+            {listVisible && (
+                <MyEvents events={events} src="list" handleBet={placeBet} handleSelect={showEventDetails} />
+            )}
+            <hr />
+            <form>
+                <MyBets bets={bets} src="bet" handleDelete={removeBar} />
+            </form>
+        </div>
+    );
+}
--- apps/sc-app-next/src/app/app.tsx
+++ apps/sc-app-next/src/app/app.tsx
@@ -1,13 +1,73 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+import React from 'react';
+import map from 'lodash/map';
 
-export function App() {
+import '@sc-app-legacy/App.css';
+import { MockContent } from './MockPubSubApp';
+import { PubSubEntity, PubSubToWebSocketsProvider } from '@sc-app-legacy/pub-sub-ws';
+import { PubSubService } from '@sc-app-legacy/PubSubService';
+import { DisplaySubs } from '@sc-app-legacy/DisplaySubs';
+import { AvailableFreebetsDevMonitor } from '@features/feature-freebets';
+import { Freebet, FreebetsProvider } from '@features/feature-freebets';
+
+function App() {
+    const bonusCreditsMock: Freebet[] = [
+        {
+            id: 1286,
+            amount: 5000,
+            promotionId: '1111',
+            friendlyDescription:
+                'You can use this Free Bet on any Football market with odds above 1.8. Only single bets are eligible.',
+            expiryDate: '2029-10-10T00:00:00.000Z',
+        },
+        {
+            id: 1287,
+            amount: 10000,
+            promotionId: '1112',
+            friendlyDescription:
+                'You can use this Free Bet on any Football market with odds above 1.8. Only single bets are eligible.',
+            expiryDate: '2024-10-10T00:00:00.000Z',
+        },
+        {
+            id: 1288,
+            amount: 20000,
+            promotionId: '1113',
+            friendlyDescription:
+                'You can use this Free Bet on any Football market with odds above 1.8. Only single bets are eligible.',
+            expiryDate: '2023-10-10T00:00:00.000Z',
+        },
+    ];
+    const eventIds = [11, 22, 33];
+    const mockEvents = map(eventIds, (id: number) => ({ id, name: `Event ${id}` }));
+    const color = 'red';
+
+    const pubSubService: PubSubService<PubSubEntity> = {
+        subscribeEvents(events: PubSubEntity[]) {
+            // console.info(`%c      SUB   +++ `, `color: ${color}`, map(events, 'id'));
+        },
+        unsubscribeEvents(events: PubSubEntity[]) {
+            // console.info(`%c      UNSUB --- `, `color: ${color}`, map(events, 'id'));
+        },
+    };
+
     return (
-        <>
-            <NxWelcome title="sc-app-next" />
-            <div />
-        </>
+        <FreebetsProvider freebets={bonusCreditsMock}>
+            <PubSubToWebSocketsProvider pubSubService={pubSubService}>
+                <table width="100%">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <DisplaySubs />
+                                <hr />
+                                <AvailableFreebetsDevMonitor />
+                            </td>
+                            <td>
+                                <MockContent fooParam={mockEvents} />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </PubSubToWebSocketsProvider>
+        </FreebetsProvider>
     );
 }
 
