cfc3f5a7a2de415c41e87da437af79dc0960c6e0
Vitalie Andries <vitalie.andries@gmail.com>
2022-12-26 16:55:31 +0200

    phase: reuse : Next app - customizing

---
+++ apps/sc-app-next/src/app/MyBets.tsx
@@ -0,0 +1,50 @@
+import React, { ReactElement, useContext, useEffect } from 'react';
+import map from 'lodash/map';
+import isEmpty from 'lodash/isEmpty';
+
+import { PubSubContext, PubSubContextType } from '@sc-app-legacy/pub-sub-ws';
+import { MockBet } from '@sc-app-legacy/test/MockEvent';
+import { MyLeg } from './MyLeg';
+
+interface MyBetsProps {
+    src: string;
+    bets: MockBet[];
+    handleDelete: (id: number) => void;
+}
+
+export function MyBets(props: MyBetsProps): ReactElement {
+    const { bets, src, handleDelete } = props;
+
+    const color = 'violet';
+
+    const { subscribeEvents, unsubscribeEvents } = useContext(PubSubContext) as PubSubContextType;
+
+    useEffect(() => {
+        if (isEmpty(bets)) {
+            return;
+        }
+        const events = map(bets, 'event');
+        subscribeEvents(events, src);
+
+        return () => {
+            unsubscribeEvents(events, src);
+        };
+    }, [bets, src, color, subscribeEvents, unsubscribeEvents]);
+
+    if (isEmpty(bets)) {
+        return (
+            <div style={{ color, textAlign: 'left' }}>
+                <h1>MyBets (none so far). Do you feel lucky?</h1>
+            </div>
+        );
+    }
+
+    return (
+        <div style={{ color, textAlign: 'left' }}>
+            <h1 data-testid="betslip-heading">MyBets ({bets.length})</h1>
+            {map(bets, (bet: MockBet) => (
+                <MyLeg key={bet.id} bet={bet} handleDelete={() => handleDelete(bet.id)} />
+            ))}
+        </div>
+    );
+}
---
+++ apps/sc-app-next/src/app/MyLeg.tsx
@@ -0,0 +1,59 @@
+import React, { ReactElement, useEffect, useState } from 'react';
+import isUndefined from 'lodash/isUndefined';
+import { isNull } from 'lodash';
+
+import * as FreebetUI from '@features/ui-legacy';
+import { Freebet, useFreebets } from '@features/feature-freebets';
+import { FreeBetContainer } from '@features/feature-freebets';
+
+import { MockBet } from '@sc-app-legacy/test/MockEvent';
+
+interface OwnProps {
+    bet: MockBet;
+    handleDelete: () => void;
+}
+
+export function MyLeg(props: OwnProps): ReactElement {
+    const { bet, handleDelete } = props;
+    const { state, getFreebet } = useFreebets();
+    const [freebet, setFreebet] = useState<Freebet | null>(null);
+
+    useEffect(() => {
+        const selectedFreebet = getFreebet(`${bet.id}`);
+
+        if (isUndefined(selectedFreebet)) {
+            setFreebet(null);
+            return;
+        }
+
+        setFreebet(selectedFreebet);
+    }, [bet.id, state.timestamp]);
+
+    return (
+        <div style={{ textAlign: 'left', display: 'flex' }} data-testid={`bet-${bet.id}`}>
+            <FreeBetContainer linkedEntityId={`${bet.id}`} UI={FreebetUI} />
+            bet: {bet.event.name}
+            {isNull(freebet) ? (
+                <input
+                    placeholder="stake"
+                    key={`input-${bet.id}`}
+                    type="text"
+                    name={`bet-${bet.id}-stake`}
+                    defaultValue=""
+                    style={{ width: '50px' }}
+                />
+            ) : (
+                <FreebetUI.Stake
+                    key={`input-${bet.id}-ro`}
+                    name={`bet-${bet.id}-stake`}
+                    value={`${freebet.amount}`}
+                    readOnly
+                />
+            )}
+            <button data-testid={`remove-bet-${bet.id}`} onClick={handleDelete}>
+                x
+            </button>
+            <hr />
+        </div>
+    );
+}
