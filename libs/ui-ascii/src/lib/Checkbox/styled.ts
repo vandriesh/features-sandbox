035b39106113bc025bc35c914a0c8341af5a7bea
Vitalie Andries <vitalie.andries@gmail.com>
2022-12-30 18:11:14 +0200

    bug: oops

--- libs/ui-ascii/src/lib/Checkbox/styled.ts
+++ libs/ui-ascii/src/lib/Checkbox/styled.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const S_Span = styled.div`
   display: inline-block;
-  padding: 1px 3px;
+  padding: 3px;
   color: black;
   font-family: monospace;
   cursor: pointer;
